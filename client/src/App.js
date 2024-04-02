096dc4d0dc005b311e814c4e8633f495edcb8d92
Alejandro Garcia <agarciacoding@gmail.com>
2024-04-01 20:17:54 -0500

    added dashboard route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React /*{useEffect, useState }*/ from 'react';
 import {BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import LandingPage from './landing-page/LandingPage';
 import SignUp from './components/SignUp';
-import Login from './components/Login'
+import Login from './components/Login';
+import Dashboard from './dashboard/Dashboard';
 
 function App ( ){
   // eslint-disable-next-line
@@ -24,6 +25,7 @@ function App ( ){
         <Route path='/' exact Component={LandingPage} />
         <Route path='/login' Component={Login} />
         <Route path='/signup' Component={SignUp} />
+        <Route path='/dashboard' Component={Dashboard} /> 
       </Routes>
     </Router>
      
