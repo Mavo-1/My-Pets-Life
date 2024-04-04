4dcb407035ee525debcf0d98a0d49ced0a0e7c2d
Alejandro Garcia <agarciacoding@gmail.com>
2024-04-04 18:43:48 -0500

    connected signup code to db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose')
 const { authToken } = require('./middleware/auth');
 require('dotenv').config()
+const authRouter = require('./routes/authRoutes');
 
-const app = express()
+const app = express();
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+app.use(cors({
+    origin: '*' // leave for development, specify for security in prod
+}));
+
+//Routes
+app.use('/auth', authRouter);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {})
-.then(() => console.log('MongoDB connected'))
+.then(() => console.log('MongoDB is connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 // Define routes
