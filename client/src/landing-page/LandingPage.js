5e0e18263b7e3f7a917929d606ea33755046e0e6
Alejandro Garcia <agarciacoding@gmail.com>
2024-05-01 14:17:06 -0500

    commented out pricing , not sure the use

--- client/src/landing-page/LandingPage.js
+++ client/src/landing-page/LandingPage.js
@@ -7,7 +7,7 @@ import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
 import Highlights from './components/Highlights';
-import Pricing from './components/Pricing';
+// import Pricing from './components/Pricing';
 import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import FAQ from './components/FAQ';
@@ -40,7 +40,7 @@ export default function LandingPage() {
         <Divider />
         <Highlights />
         <Divider />
-        <Pricing />
+        {/* <Pricing /> */}
         <Divider />
         <FAQ />
         <Divider />
