8066d5a7dfdc644176766b5fa1c6889d07be4e56
Alejandro Garcia <agarciacoding@gmail.com>
2024-05-14 18:26:31 -0500

    updates

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,7 +4,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwtSecret = "your_jwt_secret"; // Replace with your own secret key
+const jwtSecret = ""; // Replace with your own secret key
 
 module.exports = function (passport) {
   // Configure the Local Strategy for login
